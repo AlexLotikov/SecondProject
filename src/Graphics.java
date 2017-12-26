531b8a37e09ad03b9d82868610704bbf46609a8a
Alex <alexlotikov@mail.ru>
2017-12-26 10:18:00 +0300

    1 патч (графический)

---
+++ src/Graphics.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Graphics {
+
+	public static void graphics(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(1000, 500);
+		frame.setTitle("This is title from...");
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
+}
