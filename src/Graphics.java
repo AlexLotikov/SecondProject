d2bca084943a3f0035a2c870bee5856363a60b51
Alex <alexlotikov@mail.ru>
2017-12-26 16:33:14 +0300

    2 патч

--- src/Graphics.java
+++ src/Graphics.java
@@ -1,6 +1,6 @@
 import javax.swing.JFrame;
 
-public class Graphics {			//патч 1
+public class Graphics {	
 
 	public static void graphics(String[] args) {
 		JFrame frame = new JFrame();
@@ -8,11 +8,8 @@ public class Graphics {			//патч 1
 		frame.setTitle("This is title from...");
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
-<<<<<<< HEAD
+
 		frame.setVisible(true);
-=======
-		frame.setVisible(true);
-								
->>>>>>> cc0eecb7ca47ecc267701aaba69de56ac6bb973e
+
 	}
 }
